234c9d011cb919d18c56b66c98bb5ae2f969aaa4
BradlyKim <kjh5864@naver.com>
2021-12-11 12:28:08 +0900

    뷰페이저 어댑터 객체 생성 -> 실제 뷰페이저와 연결

--- app/src/main/java/com/nepplus/pizzaorderapp_20211211/MainActivity.kt
+++ app/src/main/java/com/nepplus/pizzaorderapp_20211211/MainActivity.kt
@@ -2,8 +2,13 @@ package com.nepplus.pizzaorderapp_20211211
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.nepplus.pizzaorderapp_20211211.adapters.MainViewPagerAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var mvpa : MainViewPagerAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -16,6 +21,9 @@ class MainActivity : AppCompatActivity() {
     }
     fun setValues(){
 
+        mvpa = MainViewPagerAdapter( supportFragmentManager )
+
+        mainViewPager.adapter = mvpa
 
     }
 }
