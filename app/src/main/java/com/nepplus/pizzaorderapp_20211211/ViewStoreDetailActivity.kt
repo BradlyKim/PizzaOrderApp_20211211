9dc9c8242668bfd7820cb70d7fd5593568d0f90a
BradlyKim <kjh5864@naver.com>
2021-12-11 14:36:45 +0900

    가게 상세보기 화면 추가

---
+++ app/src/main/java/com/nepplus/pizzaorderapp_20211211/ViewStoreDetailActivity.kt
@@ -0,0 +1,24 @@
+package com.nepplus.pizzaorderapp_20211211
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewStoreDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_store_detail)
+        setupEvents()
+        setValues()
+
+    }
+
+    fun setupEvents() {
+
+
+    }
+
+    fun setValues() {
+
+
+    }
+}
